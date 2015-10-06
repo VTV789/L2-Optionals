c2c74940c1074bd376ae144e79d6758cbea8e192
Vinh Thanh Vu <vtv@vinh.com>
2015-10-05 23:55:34 -0400

    Lesson 2 Exercises: Optionals Exercise 2 Solution

--- L2_Exercises.playground/Contents.swift
+++ L2_Exercises.playground/Contents.swift
@@ -24,8 +24,10 @@ if let director = moviesDict [movie] {
 /* Exercise 2
    The LoginViewController class below needs a UITextField to hold a user's name. Declare a variable for this text field as a property of the class LoginViewController. Keep in mind that the textfield property will not be initialized until after the view controller is constructed. */
 class LoginViewController: UIViewController {
-    
+    var nameTextfield:UITextField!
 }
+
+
 
 /* Exercise 3
 The class UIViewController has a property called tabBarController. The tabBarController property is an optional of type UITabBarController?. The tabBarController itslef holds a tabBar as a property. Complete the code below by filling in the approprite use of optional chaining to access tab bar property. */
